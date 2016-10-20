fa19acbf7ff5c0ba0bb4fbfff0836d240b81ab19
johnbuns <johnbuns@yahoo.com>
2016-10-20 00:29:53 -0700

    Changed REST api to use Retrofit2

--- app/src/androidTest/java/com/codemonk/introtopopularmovies/ApplicationTest.java
+++ app/src/androidTest/java/com/codemonk/introtopopularmovies/ApplicationTest.java
@@ -3,11 +3,25 @@ package com.codemonk.introtopopularmovies;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+//    public void testRestClient() throws Throwable {
+//        List<TMDBModel> test = TMDBApi.getPopular();
+//        String test2 = "";
+//
+//    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
+
